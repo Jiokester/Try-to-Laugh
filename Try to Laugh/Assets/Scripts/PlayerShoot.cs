1669dc580910f341c97af18f01b8a0d3727a3316
Jiokester <jiokerules@gmail.com>
2024-02-03 17:00:31 -0800

    Got the aiming down

---
+++ Try to Laugh/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: [Ihejirika, Chijioke]
+ * Last Updated: [02/02/2024]
+ * [Controls the player's ability to aim and shoot]
+ */
+public class PlayerShoot : MonoBehaviour
+{
+    private Camera MainCam;
+    private Vector3 mousePos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Aim();
+
+
+    }
+
+    /// <summary>
+    /// Player will aim based on the movement of the mouse
+    /// </summary>
+    private void Aim()
+    {
+        mousePos = MainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 rotation = mousePos - transform.position;
+
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+    }
+
+    private void Shoot()
+    {
+
+    }
+}
